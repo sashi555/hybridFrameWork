0f0463fcd336857e3d43d2c4822418db535e9d5d
sashi <sasisidhu123@gmail.com>
2021-09-27 15:21:43 +0530

    hybrid

---
+++ src/main/java/sashidhar/sashidhar/App.java
@@ -0,0 +1,13 @@
+package sashidhar.sashidhar;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
